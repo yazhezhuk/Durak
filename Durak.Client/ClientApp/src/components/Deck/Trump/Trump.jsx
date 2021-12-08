cbef337b72cb43680e27eedea39297703a4d5d80
Vitalich <zhukovets.vetal322@gmail.com>
2021-12-08 03:53:09 +0200

    file relocation

---
+++ Durak.Client/ClientApp/src/components/Deck/Trump/Trump.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import s from './Trump.module.css'
+
+const Trump = () =>{
+    return (
+        <div className={s.trump}>
+
+        </div>
+    )
+}
+
+export default Trump
+
