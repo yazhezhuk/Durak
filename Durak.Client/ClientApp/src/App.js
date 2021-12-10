9299da8300662c0123d32ed90ae65fe9193a8ad7
Aziull <65764457+Aziull@users.noreply.github.com>
2021-12-11 01:18:12 +0200

    commot

--- Durak.Client/ClientApp/src/App.js
+++ Durak.Client/ClientApp/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import GamePlace from './components/GamePlace/GamePlace';
 import Login from "./components/Login/Login";
+import GameList from './components/GameList/GameList';
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+        <Route exact path='/' element={<GamePlace />}/>
+        <Route path='/profile' element={ <GameList />}/>
+        <Route path='/login' element={<Login />}/>
+      </Routes>
+
     </div>
   );
 }
